7491d09ddf55fba776d95a20c79e4a94d9c11321
Abhishek gupta <abhishekgupta1997>
2021-04-19 18:12:49 +0530

    updated hostname

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req,res) => {
     // res.render('index');
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://crud-node-ejs.herokuapp.com/api/users')
     .then(function(response) {
         console.log(response)
         res.render('index', { users: response.data });
@@ -17,7 +17,7 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+    axios.get('https://crud-node-ejs.herokuapp.com/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})
         })
